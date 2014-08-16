0eca64615f2ab9466954ee785ead5c279efd72ab
Yongdae-Kim <yongdae91@hanmail.net>
2014-08-16 17:37:28 +0900

    add to comments

--- RandomGeneratorExample/src/RandomGenertor.java
+++ RandomGeneratorExample/src/RandomGenertor.java
@@ -3,14 +3,13 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-import javax.swing.JOptionPane;
-
 public class RandomGenertor {
 
 	public static void main(String[] args) {
 
 		String[] myArray = new String[] { "a", "b", "c", "d", "e", "f", "g" };
 		List<String> myRandom = new ArrayList(Arrays.asList(myArray));
+		// array to arrayList
 
 		for (int i = 0; i < myArray.length; i++) {
 
@@ -18,10 +17,12 @@ public class RandomGenertor {
 
 			System.out.println("get : " + myRandom.get(randomNum));
 			System.out.println("remove : " + myRandom.remove(randomNum));
+			// deduplication
 			System.out.println(myRandom);
 
 			if (myRandom.size() == 0) {
 				System.out.println("nothing");
+				// need to reassignment
 			}
 		}
 	}
