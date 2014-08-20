57b2a7d08e666ab9cceb4882407f0e84dd29a9a5
Yongdae-Kim <yongdae91@hanmail.net>
2014-08-20 13:13:24 +0900

    change

--- FibonaccRecursion/src/kr/ac/jejunu/softlab/fibonacci/main/Main.java
+++
@@ -1,9 +0,0 @@
-package kr.ac.jejunu.softlab.fibonacci.main;
-
-import kr.ac.jejunu.softlab.fibonacci.logic.Fibonacci;
-
-public class Main {
-	public static void main(String[] args) {
-		new Fibonacci().example(10);
-	}
-}
