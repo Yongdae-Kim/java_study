c34e0cb81afbce4f337e55a3c7874e811587af8c
Scratchback <yongdae91@hanmail.net>
2015-04-11 09:57:58 +0900

    revision project

--- Cafeteria/src/kr/ac/jejunu/softlab/cafeteria/menu/JejuUnivCafeteriaHtmlParser.java
+++ Cafeteria/src/kr/ac/jejunu/softlab/cafeteria/menu/JejuUnivCafeteriaHtmlParser.java
@@ -5,28 +5,24 @@ import java.net.URL;
 
 import kr.ac.jejunu.softlab.cafeteria.callback.CallBack;
 import net.htmlparser.jericho.Element;
-import net.htmlparser.jericho.HTMLElementName;
 import net.htmlparser.jericho.Source;
 
 public class JejuUnivCafeteriaHtmlParser {
 
 	private final String url = "http://www.jejunu.ac.kr/_2014/camp/stud/01.jsp";
-	private Element div;
+
+	private Source source;
 
 	public JejuUnivCafeteriaHtmlParser(CallBack callBack) {
-		Source source = null;
 		try {
 			source = new Source(new URL(url));
 			source.fullSequentialParse();
 		} catch (IOException e) {
 			callBack.CallBackMethod(e);
 		}
-
-		Element body = source.getAllElements(HTMLElementName.BODY).get(0);
-		div = body.getAllElements(HTMLElementName.DIV).get(6);
 	}
 
 	public Element getElement() {
-		return div.getAllElements(HTMLElementName.DIV).get(3);
+		return source.getElementById("Content");
 	}
 }
