a62e9f9d81a986232c5ec02659944d35f06d8813
Yongdae-Kim <yongdae91@hanmail.net>
2015-10-01 09:38:33 +0900

    Update Fibonacci.java

--- FibonacciRecursion/src/kr/ac/jejunu/softlab/fibonacci/logic/Fibonacci.java
+++ FibonacciRecursion/src/kr/ac/jejunu/softlab/fibonacci/logic/Fibonacci.java
@@ -3,20 +3,17 @@ package kr.ac.jejunu.softlab.fibonacci.logic;
 public class Fibonacci {
 
 	public void example(int ex) {
-
+		
 		for (int i = 0; i < ex; i++)
-
-			System.out.println(new Fibonacci().function(i));
+		
+			System.out.println(function(i));
 	}
 
 	private int function(int num) {
 
 		if (num == 0 || num == 1)
-			return 0;
+			return num;
 		// zero, first of fibonacci number
-		else if (num == 2)
-			return 1;
-		// second of fibonacci number
 		else
 			return function(num - 1) + function(num - 2);
 		// after third of the fibonacci number
